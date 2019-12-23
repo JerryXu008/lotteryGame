88108a507a7dd180e92bb19e76e22a80fd84c3c3
JerryXu <xujinleixiaonanxia@163.com>
2019-12-23 21:50:52 +0800

    t

--- js/awards.js
+++ js/awards.js
@@ -2,17 +2,17 @@ window.awards =  [
     {
      name: '二等奖',
      award: '办公室一日游',
-     count: 5,
+     count: 500,
     }, 
    {
        name: '一等奖',
        award: 'BMW X5',
-       count: 3,
+       count: 500,
      },
      {
        name: '特等奖',
        award: 'iPhone 11 Pro Max',
-       count: 1,
+       count: 500,
      },
    ];
 
